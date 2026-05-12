7b475465edde66999166f090571cc8e920ef26d2
Descent Chapfuwa <descenttf@gmail.com>
2026-05-12 15:12:13 +0200

    Finished setting up the project dependencies and modular monolith  architectural packages

---
+++ backend/src/main/java/com/techsensei/payment_intergration_system/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.techsensei.payment_intergration_system.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
