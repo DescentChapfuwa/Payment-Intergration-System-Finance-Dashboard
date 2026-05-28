6ba73cf9ca672ebe2935bd1364a2e2251c08fb41
Descent Chapfuwa <descenttf@gmail.com>
2026-05-28 16:23:43 +0200

    Corrected a query error

--- backend/src/main/java/com/techsensei/payment_intergration_system/backend/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/techsensei/payment_intergration_system/backend/notification/repository/NotificationRepository.java
@@ -3,6 +3,7 @@ package com.techsensei.payment_intergration_system.backend.notification.reposito
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.techsensei.payment_intergration_system.backend.notification.entity.Notification;
 
@@ -10,6 +11,12 @@ public interface NotificationRepository extends JpaRepository<Notification,Long>
 
     Page<Notification>findByUserIdOrderByCreatedAtDesc(Long userId,Pageable pageable);
 
+    @Query("""
+       SELECT COUNT(n)
+       FROM Notification n
+       WHERE n.user.id = :userId
+       AND n.isRead = FALSE
+       """)
     long countByUserIdAndReadFalse(Long userId);
 
 }
